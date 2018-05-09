2f521d9a501101f461aa3735782b579b7aee8554
Sifon98 <alexander98@comhem.se>
2018-05-09 13:52:32 +0200

    added geo function and various fixes

--- js/2.js
+++ js/2.js
@@ -1,3 +1,95 @@
+function getLocation() {
+    
+   geocoder = new google.maps.Geocoder();
+
+       if (navigator.geolocation) {
+           navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+       }
+       function successFunction(position) {
+           var lat = position.coords.latitude;
+           var lng = position.coords.longitude;
+           codeLatLng(lat, lng)
+       }
+
+       function errorFunction() {
+           alert("Geocoder failed! Type location in the searchfield.");
+       }
+
+       function initialize() {
+           geocoder = new google.maps.Geocoder();
+       }
+
+       function codeLatLng(lat, lng) {
+
+           var latlng = new google.maps.LatLng(lat, lng);
+           geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+               if (status == google.maps.GeocoderStatus.OK) {
+                   //console.log(results);
+                   if (results[1]) {
+                       var indice = 0;
+                       for (var j = 0; j < results.length; j++) {
+                           if (results[j].types[0] == 'locality') {
+                               indice = j;
+                               break;
+                           }
+                       }
+                       console.log(results[j]);
+                       for (var i = 0; i < results[j].address_components.length; i++) {
+                           if (results[j].address_components[i].types[0] == "locality") {
+                               city = results[j].address_components[i];
+                           }
+                       }
+                       document.getElementById("searchName").innerHTML = city.long_name;
+                       document.getElementById("name").innerHTML = city.long_name;
+                       Cookies.set('_username', city.long_name);
+                       location.reload();
+                    //    document.getElementById("name").innerHTML = city.long_name;
+                   } else {
+                       alert("No results found");
+                   }
+               } else {
+                   alert("Geocoder failed due to: " + status);
+               }
+           });
+       }
+       
+       const apiKey = 'pppt4';
+       const fetchSoch = async (url) => await (await fetch(url, { headers: new Headers({ 'Accept': 'application/json' }) })).json();
+       // funktion för att göra en enkel sökning
+       const sochSearch = async (query) => {
+           const json = await fetchSoch(`http://kulturarvsdata.se/ksamsok/api?x-api=${apiKey}&method=search&query=${query}&recordSchema=presentation`);
+           return json.result.records.record.map(r => r['pres:item']);
+       };
+       // Hämta ett object via en URI (JSON-LD)
+       fetchSoch('http://kulturarvsdata.se/raa/bbrp/21620000011565').then(data => {
+           // console.log(data);
+       }).catch(reason => {
+           console.error(reason.message);
+       });
+       // här kan du också skriva CQL frågor (item="sten yxa" AND place=gotland)
+        (sochSearch(name).then(data => {
+            console.log(people)
+           // loopa igenom snygg fin JavaScript array med listade object
+            for (i=0;i<data.length; i++){
+               var record = data[i];
+               // logga objekt
+               console.log(record);
+               // skriv ut specifikt element ur objektet
+               // console.log(record['pres:dataQuality']);
+               // skriv ut specifikt element till HTML
+               // document.querySelector('.elementId1').innerText = record['pres:itemLabel'];
+               people.push({title: record['pres:itemLabel'], description: record['pres:description'], image: record['pres:image']['pres:src'][0]['content'],
+       
+               link: record['pres:representations']['pres:representation'][0]['content']});
+       
+               // document.querySelector('.elementId2').src = record['pres:image']['pres:src'][0]['content'];
+               // document.querySelector('.elementId2').value = record['pres:image']['pres:src'][0]['type'];
+               // document.querySelector('.elementId3').href = record['pres:representations']['pres:representation'][0]['content'];
+               // document.querySelector('.elementId4').innerText = record['pres:description'];
+               
+           }}));
+}
+
 $(document).ready(function () {
     var name = Cookies.get('_username');
     if (name) {
@@ -8,7 +100,8 @@ $(document).ready(function () {
         Cookies.set('_username', inputName);
     })
    
-    console.log(name);
+    var searchName = name;
+    document.getElementById('searchName').innerHTML = searchName;
 
 const apiKey = 'pppt4';
 const fetchSoch = async (url) => await (await fetch(url, { headers: new Headers({ 'Accept': 'application/json' }) })).json();
@@ -45,8 +138,4 @@ fetchSoch('http://kulturarvsdata.se/raa/bbrp/21620000011565').then(data => {
         // document.querySelector('.elementId4').innerText = record['pres:description'];
         
     }}));
-//  }).catch(reason => {
-//      // nätverksfel av någon typ
-//      console.error(reason.message);
-//      });
 });
